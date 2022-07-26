25c7131e02543c4bec9a9506ced00ff0f6e70718
FreakyCoder <kurayogun@gmail.com>
2022-07-27 00:51:03 +0300

    chore: bounceable version 1.0 WIP

--- example/App.tsx
+++ example/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { View, ScrollView, StatusBar, SafeAreaView } from "react-native";
+import {
+  View,
+  ScrollView,
+  StatusBar,
+  Text,
+  SafeAreaView,
+  Alert,
+} from "react-native";
 import ProfileHeader from "react-native-profile-header";
 import IGStoryCircle from "react-native-instagram-story-circle";
 import { AppleCard, AppOfTheDayCard } from "react-native-apple-card-views";
+import RNBounceable from "./lib/RNBounceable";
 
 const coloredNotification = require("./assets/notification-colored.png");
 const notification = require("./assets/notification.png");
@@ -14,8 +22,7 @@ const stories = [
     storyRingColor: ["#20fab1", "#20fab1"],
     notificationCount: 3,
     source: {
-      uri:
-        "https://images.unsplash.com/uploads/14110635637836178f553/dcc2ccd9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+      uri: "https://images.unsplash.com/uploads/14110635637836178f553/dcc2ccd9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     },
   },
   {
@@ -24,8 +31,7 @@ const stories = [
     storyRingColor: ["#eb3434", "#eb3434"],
     isStoryRead: true,
     source: {
-      uri:
-        "https://images.unsplash.com/photo-1496440737103-cd596325d314?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+      uri: "https://images.unsplash.com/photo-1496440737103-cd596325d314?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
     },
   },
   {
@@ -33,8 +39,7 @@ const stories = [
     hasStory: true,
     storyRingColor: ["#e22ae8", "#e22ae8"],
     source: {
-      uri:
-        "https://images.unsplash.com/photo-1514846226882-28b324ef7f28?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+      uri: "https://images.unsplash.com/photo-1514846226882-28b324ef7f28?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
     },
   },
   {
@@ -43,8 +48,7 @@ const stories = [
     storyRingColor: ["#3492eb", "#3492eb"],
     notificationCount: 16,
     source: {
-      uri:
-        "https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+      uri: "https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     },
   },
   {
@@ -52,24 +56,21 @@ const stories = [
     hasStory: true,
     notificationCount: 1,
     source: {
-      uri:
-        "https://images.unsplash.com/photo-1448376561459-dbe8868fa34c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+      uri: "https://images.unsplash.com/photo-1448376561459-dbe8868fa34c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     },
   },
   {
     key: "Dapibus Euismod",
     hasStory: true,
     source: {
-      uri:
-        "https://images.unsplash.com/photo-1539811107033-3a67f3ebc852?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80",
+      uri: "https://images.unsplash.com/photo-1539811107033-3a67f3ebc852?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80",
     },
   },
   {
     key: "Pellentesque",
     hasStory: true,
     source: {
-      uri:
-        "https://images.unsplash.com/photo-1551292831-023188e78222?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+      uri: "https://images.unsplash.com/photo-1551292831-023188e78222?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     },
   },
 ];
@@ -144,6 +145,20 @@ const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
+      <RNBounceable
+        style={{
+          height: 150,
+          width: 300,
+          backgroundColor: "red",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        onPress={() => {
+          alert("hey");
+        }}
+      >
+        <Text style={{ color: "#fff", fontSize: 32 }}>Hello</Text>
+      </RNBounceable>
       <SafeAreaView>
         {renderHeader()}
         {renderIGStories()}
